b0079adddaec852d033107c2a139aea65ba8ff49
Fernando Barbosa <mail@fdr.one>
2021-02-21 11:28:59 -0300

    Fix clj reverse string documentation

--- clojure/reverse-string/src/reverse_string.clj
+++ clojure/reverse-string/src/reverse_string.clj
@@ -2,7 +2,7 @@
 (require '[clojure.string :as string])
 
 (defn reverse-index-list
-  "Given a string, returns a list of sequential natural numbers [N, N-1, N-2, ..., 0], where N equals the total number of characters of that string"
+  "Given a string, returns a list of sequential natural numbers [N-1, N-2, ..., 0], where N equals the total number of characters of that string"
   [s]
   (range (dec (count s)) -1 -1))
 
