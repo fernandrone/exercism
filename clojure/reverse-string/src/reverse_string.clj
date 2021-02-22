a9c87f73c8ea4fa345c4e98511335a0ceae415d8
Fernando Barbosa <mail@fdr.one>
2021-02-22 10:36:11 -0300

    Update reverse-string to use 'into'

--- clojure/reverse-string/src/reverse_string.clj
+++ clojure/reverse-string/src/reverse_string.clj
@@ -1,10 +1,5 @@
-(ns reverse-string)
-(require '[clojure.string :as string])
-
-(defn reverse-index-list
-  "Given a string, returns a list of sequential natural numbers [N-1, N-2, ..., 0], where N equals the total number of characters of that string"
-  [s]
-  (range (dec (count s)) -1 -1))
+(ns reverse-string
+  (:require [clojure.string :as string]))
 
 (defn reverse-string [s]
-  (string/join (map #(nth s %) (reverse-index-list s))))
+  (string/join (into () s)))
