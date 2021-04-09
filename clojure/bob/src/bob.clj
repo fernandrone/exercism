ce43787463f1a63ad6878b775d986b32e597cd97
Fernando Barbosa <mail@fdr.one>
2021-04-09 18:43:05 -0300

    Address review comments for bob

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,22 +1,21 @@
 (ns bob
   (:require [clojure.string :as string]))
 
-(defn is-shout? [s]
+(defn shout? [s]
   (some->> s
            (filter #(Character/isLetter %))
            (not-empty)
            (every? #(Character/isUpperCase %))))
 
-(defn is-question? [s]
+(defn question? [s]
   (string/ends-with? (string/trimr s) "?"))
 
-(defn is-silence? [s]
-  (string/blank? s))
+(def silence? string/blank?)
 
 (defn response-for [s]
-  (condp apply [s]
-    (every-pred is-question? is-shout?) "Calm down, I know what I'm doing!"
-    is-question? "Sure."
-    is-shout?    "Whoa, chill out!"
-    is-silence?  "Fine. Be that way!"
-    "Whatever."))
+  (cond
+    ((every-pred question? shout?) s) "Calm down, I know what I'm doing!"
+    (question? s) "Sure."
+    (shout? s)    "Whoa, chill out!"
+    (silence? s)  "Fine. Be that way!"
+    :else "Whatever."))
