cb72298c1b9ee19bf6edb815c65194d9e34a51b8
Fernando Barbosa <mail@fdr.one>
2021-06-09 20:54:08 -0300

    Update collatz-conjecture

--- clojure/collatz-conjecture/src/collatz_conjecture.clj
+++ clojure/collatz-conjecture/src/collatz_conjecture.clj
@@ -5,3 +5,13 @@
     (= n 1) 0
     (even? n) (+ 1 (collatz (/ n 2)))
     :else (+ 1 (collatz (inc (* 3 n))))))
+
+
+(defn collatz-recur [n]
+  (if (< n 1)
+    (throw (IllegalArgumentException. "Number must be positive!"))
+    (loop [n n, acc 0]
+      (cond
+        (= n 1) acc
+        (even? n) (recur (/ n 2) (inc acc))
+        :else (recur (inc (* 3 n)) (inc acc))))))
