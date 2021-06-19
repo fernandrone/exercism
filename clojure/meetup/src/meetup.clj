4e61f2367993b24073fc512ff4532ed6c17cec3b
Fernando Barbosa <mail@fdr.one>
2021-06-19 20:30:07 -0300

    Updated solution to meetup

--- clojure/meetup/src/meetup.clj
+++ clojure/meetup/src/meetup.clj
@@ -11,13 +11,16 @@
    :friday    DayOfWeek/FRIDAY
    :saturday  DayOfWeek/SATURDAY})
 
+(defn- adjust [date wd descriptor]
+  (cond-> date
+    (= descriptor :first)  (.with (TemporalAdjusters/firstInMonth wd))
+    (= descriptor :second) (.with (TemporalAdjusters/dayOfWeekInMonth 2 wd))
+    (= descriptor :third)  (.with (TemporalAdjusters/dayOfWeekInMonth 3 wd))
+    (= descriptor :fourth) (.with (TemporalAdjusters/dayOfWeekInMonth 4 wd))
+    (= descriptor :last)   (.with (TemporalAdjusters/lastInMonth wd))
+    (= descriptor :teenth) (.with (TemporalAdjusters/next wd))))
+
 (defn meetup [month year weekday descriptor]
-  (let [wd (weekday->enum weekday)]
-    (cond-> (LocalDate/parse (format "%04d-%02d-12" year month))
-      (= descriptor :first)  (.with (TemporalAdjusters/firstInMonth wd))
-      (= descriptor :second) (.with (TemporalAdjusters/dayOfWeekInMonth 2 wd))
-      (= descriptor :third)  (.with (TemporalAdjusters/dayOfWeekInMonth 3 wd))
-      (= descriptor :fourth) (.with (TemporalAdjusters/dayOfWeekInMonth 4 wd))
-      (= descriptor :last)   (.with (TemporalAdjusters/lastInMonth wd))
-      (= descriptor :teenth) (.with (TemporalAdjusters/next wd))
-      :always ((juxt #(.getYear %) #(.getMonthValue %) #(.getDayOfMonth %))))))
+  (-> (LocalDate/parse (format "%04d-%02d-12" year month))
+      (adjust (weekday->enum weekday) descriptor)
+      ((juxt #(.getYear %) #(.getMonthValue %) #(.getDayOfMonth %)))))
