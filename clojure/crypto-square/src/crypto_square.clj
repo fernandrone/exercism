7e91732f9efa5d4c0f8d5e16803366397a142edb
Fernando Barbosa <mail@fdr.one>
2021-06-19 14:56:10 -0300

    Finish crypt_square

--- clojure/crypto-square/src/crypto_square.clj
+++ clojure/crypto-square/src/crypto_square.clj
@@ -2,21 +2,45 @@
   (:require [clojure.string :as str]))
 
 (defn normalize-plaintext [s]
-  (str/replace s #"[^a-zA-Z]" ""))
+  (-> (str/lower-case s)
+      (str/replace #"[^0-9a-zA-Z]" "")))
 
 (defn square-size [s]
   (->> (iterate inc 1)
-       (drop-while #(< (* % %) (count s)))
+       (drop-while #(< (* % %) (count (normalize-plaintext s))))
        (first)))
 
-(defn plaintext-segments [] ;; <- arglist goes here
-  ;; your code goes here
-  )
+(defn plaintext-segments [s]
+  (->> (normalize-plaintext s)
+       (re-seq (re-pattern (format ".{1,%d}" (square-size s))))))
 
-(defn ciphertext [] ;; <- arglist goes here
-  ;; your code goes here
-  )
+(defn ciphertext [s]
+  (let [s (plaintext-segments s)]
+    (loop [square s, acc ""]
+      (if (every? empty? square)
+        acc
+        (recur
+         (map #(subs % (min 1 (count %))) square)
+         (str/join (conj (map #(get % 0) square) acc)))))))
 
-(defn normalize-ciphertext [] ;; <- arglist goes here
-  ;; your code goes here
-  )
+(defn row-size [s]
+  (->> (iterate inc 1)
+       (drop-while #(< (* % (inc %)) (count (normalize-plaintext s))))
+       (first)))
+
+(defn normalize-ciphertext [s]
+  (let [c      (square-size s), r (row-size s)
+        len    (count (normalize-plaintext s))
+        cipher (ciphertext s)
+        split (- (* r c) len)
+        begin  (->> cipher
+                    (re-seq (re-pattern (format ".{1,%d}" r)))
+                    (take (- c split)))
+        end    (->> cipher
+                    (str/reverse)
+                    (re-seq (re-pattern (format ".{1,%d}" (dec r))))
+                    (take split)
+                    (map str/reverse)
+                    (reverse))]
+    (->> (concat begin end)
+         (str/join " "))))
