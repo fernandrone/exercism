45ac309c4ec7a04d0130f7130f19a45474dc543c
Fernando Barbosa <mail@fdr.one>
2021-06-27 17:45:58 -0300

    Second solution to rna problem

--- clojure/rna-transcription/src/rna_transcription.clj
+++ clojure/rna-transcription/src/rna_transcription.clj
@@ -1,34 +1,13 @@
 (ns rna-transcription
   (:require [clojure.string :as str]))
 
-(defn- dna->rna
-  "Converts a DNA nucleotide string to a RNA nucleotide string."
-  [nucleotide]
-  (case nucleotide
-    "G" "C"
-    "C" "G"
-    "T" "A"
-    "A" "U"))
 
-(defn- dna?
-  "Returns true if the nucleotide is a valid DNA strand:
-   a string of value G, C, T or A."
-  [nucleotide]
-  (case nucleotide
-    "G" true
-    "C" true
-    "T" true
-    "A" true
-    false))
-
-(defn- dna->coll
-  "Converts a DNA string into a lazy sequence of its individual
-   DNA nucleotides."
-  [dna]
-  (re-seq #"\w{1}" dna))
+(def ^:private dna->rna
+  {\G \C
+   \C \G
+   \T \A
+   \A \U})
 
 (defn to-rna [dna]
-  {:pre [(every? dna? (dna->coll dna))]}
-  (->> (dna->coll dna)
-       (map dna->rna)
-       (str/join)))
+  {:post [(= (count dna) (count %))]}
+  (str/join (map dna->rna (char-array dna))))
